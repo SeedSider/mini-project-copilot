f61aeabf7654d223248836b94bdea05159e546cf
SeedSider <sidiqusman13@gmail.com>
2026-03-30 18:01:12 +0700

    feat: Update GetEnv function to check for non-empty environment variable values

--- bff-service/server/core_config.go
+++ bff-service/server/core_config.go
@@ -67,7 +67,7 @@ func initConfig() {
 }
 
 func GetEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
+	if value, ok := os.LookupEnv(key); ok && value != "" {
 		return value
 	}
 	return fallback
--- user-profile-service/server/core_config.go
+++ user-profile-service/server/core_config.go
@@ -33,7 +33,7 @@ func initConfig() {
 
 // GetEnv reads an environment variable with a fallback default.
 func GetEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
+	if value, ok := os.LookupEnv(key); ok && value != "" {
 		return value
 	}
 	return fallback
